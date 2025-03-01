d98c6483f17b9b14cdf72f2fd63f66a1e2ecc87c
Markneoneo <markneoneo@gmail.com>
2025-03-01 20:54:56 +0800

    Fixed import issues

--- src/test/java/Javatro/PokerHandTest.java
+++ src/test/java/Javatro/PokerHandTest.java
@@ -1,6 +1,6 @@
 package Javatro;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import java.util.List;
 
