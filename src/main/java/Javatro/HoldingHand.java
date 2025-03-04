5b4d3a83a9970e0d8e44b32b48294efcfb7754eb
john <john.woo.yk@gmail.com>
2025-03-04 21:42:55 +0800

    Add functionality of Decks, HoldingHand
    Deck.java: Holds the deck of cards. User is able to draw 1 card, and query remaining cards in the deck.
    HoldingHand.java: Holds the cards the user is given to play. User is able to play a set number of cards, or discard a set number of cards.

---
+++ src/main/java/Javatro/HoldingHand.java
@@ -0,0 +1,63 @@
+package Javatro;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * Tracks the 8 cards held in a hand
+ * Handles the draw function and discard function
+ */
+
+public class HoldingHand {
+    private final int HOLDINGLIMIT = 8; // The maximum number of cards a hand can hold
+    protected List<Card> Hand;
+
+    public HoldingHand(Deck deck) {
+        Hand = new ArrayList<Card>();
+        for (int i = 0; i < HOLDINGLIMIT; i++) {
+            Hand.add(deck.draw());
+        }
+    }
+
+    /**
+     * Returns the specified cards as requested by the player Hand will then draw the same number of
+     * cards back from the deck
+     *
+     * <p>This function should not be called if there are no cards played.
+     *
+     * @param plays List containing cards to be played
+     * @param deck Deck containing the remaining cards
+     */
+    public List<Card> play(List<Integer> plays, Deck deck) {
+        List<Card> playList = new ArrayList<Card>();
+        for (int play : plays) {
+            Card card = Hand.remove(play);
+            playList.add(card);
+        }
+
+        for (int i = 0; i < plays.size(); i++) {
+            Card tempCard = deck.draw();
+            Hand.add(tempCard);
+        }
+        return playList;
+    }
+
+    /**
+     * Discards a set number of cards. This function should not be used if the number of discards
+     * left is 0.
+     *
+     * @param discards List containing the cards at specified positions to be discarded
+     * @param deck Deck containing the remaining cards
+     */
+    public void discard(List<Integer> discards, Deck deck) {
+        for (int discard : discards) {
+            Hand.remove(discard);
+        }
+
+        // Draw the same number of cards discarded
+        for (int i = 0; i < discards.size(); i++) {
+            Card tempCard = deck.draw();
+            Hand.add(tempCard);
+        }
+    }
+}
---
+++ src/test/java/Javatro/HoldingHandTest.java
@@ -0,0 +1,32 @@
+package Javatro;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class HoldingHandTest {
+
+    @Test
+    void testDeck() {
+        Deck deck = new Deck();
+        int cardsRemaining = deck.getRemainingCards();
+        assertEquals(52, cardsRemaining);
+        Card drawOne = deck.draw();
+        assertEquals(51, deck.getRemainingCards());
+    }
+
+    @Test
+    void testHoldingHand() {
+        Deck deck = new Deck();
+        HoldingHand holdingHand = new HoldingHand(deck);
+        int cardsRemaining = deck.getRemainingCards();
+        assertEquals(44, cardsRemaining);
+        holdingHand.discard(List.of(1, 3, 5), deck);
+        assertEquals(41, deck.getRemainingCards());
+        List<Card> playedHand = holdingHand.play(List.of(1, 4, 5), deck);
+        assertEquals(3, playedHand.size());
+        assertEquals(38, deck.getRemainingCards());
+    }
+}
