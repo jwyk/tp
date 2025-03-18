790060227db814d665ffd207089097d1594c0a8b
swethacool <swetha2294cool@gmail.com>
2025-03-18 22:50:38 +0800

    edited the GOODBYE message to print out properly

--- src/main/java/javatro_manager/ExitGameCommand.java
+++ src/main/java/javatro_manager/ExitGameCommand.java
@@ -23,11 +23,11 @@ public class ExitGameCommand implements Command {
     @Override
     public void execute() {
         System.out.println("================================================");
-        System.out.println(" GGGG   OOO   OOO   DDDD    BBBB   Y   Y  EEEEE ");
+        System.out.println(" GGGG    OOO   OOO   DDDD    BBBB   Y   Y  EEEEE ");
         System.out.println("G       O   O O   O  D   D   B   B   Y Y   E     ");
         System.out.println("G  GG   O   O O   O  D   D   BBBB     Y    EEEE  ");
         System.out.println("G   G   O   O O   O  D   D   B   B    Y    E     ");
-        System.out.println(" GGGG   OOO   OOO   DDDD    BBBB     Y    EEEEE ");
+        System.out.println(" GGGG    OOO   OOO   DDDD    BBBB     Y    EEEEE ");
         System.out.println("================================================");
         System.out.println("      Thanks for playing! See you next time!     ");
         System.out.println("================================================");
