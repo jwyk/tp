f2dcd2fcab8ac4b78d5bffb47a6098f410346ab2
Auto Formatter <auto_formatter@gmail.com>
2025-03-23 16:01:09 +0000

    Apply google-java-format to Java files

--- src/test/java/javatro/core/RoundTest.java
+++ src/test/java/javatro/core/RoundTest.java
@@ -1,10 +1,10 @@
 package javatro.core;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 public class RoundTest {
     private void assertRoundInitialization(int blindScore, int remainingPlays)
