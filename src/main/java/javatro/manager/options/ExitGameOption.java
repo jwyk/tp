b84d937b55514610450237cc1d896ca15515a3cc
john <john.woo.yk@gmail.com>
2025-03-24 00:55:15 +0800

    Fix static variable declaration order

--- src/main/java/javatro/manager/options/ExitGameOption.java
+++ src/main/java/javatro/manager/options/ExitGameOption.java
@@ -14,6 +14,7 @@ import java.util.Scanner;
  * prints a farewell message and exits the application.
  */
 public class ExitGameOption implements Option {
+    private static String END_SCREEN;
 
     /**
      * Provides a brief description of the command.
@@ -25,7 +26,6 @@ public class ExitGameOption implements Option {
         return "Exit Game";
     }
 
-    private static String END_SCREEN;
 
     // Static block to initialize the End Screen from a file
     static {
