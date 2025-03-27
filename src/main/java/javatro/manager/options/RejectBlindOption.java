377d2e1a08ed9c3a68930dd4be44b100c618555e
swethacool <swetha2294cool@gmail.com>
2025-03-27 12:52:08 +0800

    RejectBlindOption class handles the error warning that is thrown if the player tries to bypass the BOSS round which is compulsory in Javatro and it also ensures player has the ability to skip the large and small blinds that are optional in this game

--- src/main/java/javatro/manager/options/RejectBlindOption.java
+++ src/main/java/javatro/manager/options/RejectBlindOption.java
@@ -6,6 +6,8 @@ import javatro.core.JavatroException;
 import javatro.display.UI;
 import javatro.manager.JavatroManager;
 
+import java.util.List;
+
 public class RejectBlindOption implements Option {
     @Override
     public String getDescription() {
@@ -14,14 +16,23 @@ public class RejectBlindOption implements Option {
 
     @Override
     public void execute() throws JavatroException {
-        if(JavatroCore.ante.getBlind() == Ante.Blind.SMALL_BLIND){
-            JavatroCore.ante.setBlind(Ante.Blind.LARGE_BLIND);
+        if(JavatroManager.ante.getBlind() == Ante.Blind.SMALL_BLIND){
+            JavatroManager.ante.setBlind(Ante.Blind.LARGE_BLIND);
             JavatroManager.setScreen(UI.getBlindScreen());
         }
-        else if(JavatroCore.ante.getBlind() == Ante.Blind.LARGE_BLIND){
-            JavatroCore.ante.setBlind(Ante.Blind.BOSS_BLIND);
-            JavatroCore.currentRound.updateRoundVariables();
-            JavatroManager.setScreen(UI.getGameScreen());
+        else if(JavatroManager.ante.getBlind() == Ante.Blind.LARGE_BLIND){
+            JavatroManager.ante.setBlind(Ante.Blind.BOSS_BLIND);
+            JavatroManager.setScreen(UI.getBlindScreen());
+        }
+        else{
+            String title = "⚠\uFE0F " + UI.BOLD + "WARNING" + " ⚠\uFE0F" + UI.END;
+
+            String[] lines = {
+                    "Oops! you have to play the BOSS blind to proceed, no skipping! :D",
+            };
+
+            UI.printBorderedContent(title, List.of(lines));
+            JavatroManager.setScreen(UI.getBlindScreen());
         }
     }
 }
