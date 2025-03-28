7c6e3609909180b290e55145b99697efd83f12a1
john <john.woo.yk@gmail.com>
2025-03-29 01:04:51 +0800

    Resolve merge conflict and add OddTodd joker

---
+++ src/main/java/javatro/core/jokers/addchip/OddToddJoker.java
@@ -0,0 +1,31 @@
+package javatro.core.jokers.addchip;
+
+import javatro.core.Card;
+import javatro.core.Score;
+import javatro.core.jokers.Joker;
+
+/**
+ * Represents a OddToddJoker Joker
+ */
+public class OddToddJoker extends Joker {
+
+    public OddToddJoker(int numberOfJokers) {
+        super();
+        this.description =
+                "+3 Mult for each Joker card held." + "Current Multiplier: " + numberOfJokers;
+        this.scoreType = ScoreType.AFTERPLAYHAND;
+    }
+
+    @Override
+    public void interact(Score scoreClass, Card playedCard) {
+        this.description = "Played cards with odd rank gives +31 Chips when scored [A,9,7,5,3].";
+        if ((playedCard.getChips()) % 2 != 0) {
+            scoreClass.totalChips += 31;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "+31 Chips if Odd";
+    }
+}
