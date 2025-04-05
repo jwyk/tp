ed5c50ea864e50ccb7b483f7f8846e7b1d38eedb
john <john.woo.yk@gmail.com>
2025-04-05 16:59:18 +0800

    Add package info for joker package

---
+++ src/main/java/javatro/core/jokers/package-info.java
@@ -0,0 +1,15 @@
+package javatro.core.jokers;
+
+/**
+ * The {@code jokers} package contains the joker logic and data structures necessary for instantiating and creating Jokers.
+ *
+ * <p>This package is responsible for handling the distribution and logic of Jokers.
+ *
+ * <h2>Classes in this package:</h2>
+ *
+ * <ul>
+ *   <li>{@link javatro.core.jokers.Joker} - Represents a {@code Joker} abstract class.
+ *   <li>{@link javatro.core.jokers.JokerFactory} - Manages the creation of Jokers.
+ *   <li>{@link javatro.core.jokers.HeldJokers} - Represents the player's current Jokers.
+ * </ul>
+ */
