ce75be8d35aabc4600cee6a8189fee649ab0795d
Auto Formatter <auto_formatter@gmail.com>
2025-04-07 14:21:46 +0000

    Apply google-java-format to Java files

--- src/main/java/javatro/Javatro.java
+++ src/main/java/javatro/Javatro.java
@@ -4,9 +4,7 @@ import javatro.audioplayer.AudioPlayer;
 import javatro.core.JavatroCore;
 import javatro.core.JavatroException;
 import javatro.display.UI;
-import javatro.display.screens.LoseScreen;
 import javatro.display.screens.StartScreen;
-import javatro.display.screens.WinGameScreen;
 import javatro.manager.JavatroManager;
 
 /**
